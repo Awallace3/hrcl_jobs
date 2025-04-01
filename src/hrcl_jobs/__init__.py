f2bb988180962b82b3a4a945df2550dbf321eacd
AMW Arch <awallace43@gatech.edu>
2025-04-01 19:44:51 -0400

    updates

--- src/hrcl_jobs/__init__.py
+++ src/hrcl_jobs/__init__.py
@@ -2,10 +2,12 @@ import warnings
 from . import sqlt
 from . import serial
 from . import jobspec
+import_error = None
 try:
     from . import pgsql
 except ImportError:
     warnings.warn("Could not import all modules from hrcl_jobs.pgsql (install psycopg2)")
+    import_error = f"{ImportError}"
     pass
 try:
     from . import parallel
@@ -14,10 +16,13 @@ try:
     from . import utils
 except ImportError:
     # warnings.warn("Could not import all modules from hrcl_jobs.parallel (install mpi4py)")
+    import_error = f"{ImportError}"
     pass
 
 try:
     from . import dataset 
 except ImportError:
     # warnings.warn("Could not import all modules from hrcl_jobs.dataset (install mpi4py and psi4)")
+    import_error = f"{ImportError}"
     pass
+
